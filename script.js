c927e6a9868dc32fad96b458388a4cda9be0a12a
Piyush Toraskar <22ee02004@iitbbs.ac.in>
2025-08-15 19:02:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-function toggleDarkMode() {
-    document.body.classList.toggle('dark');
-    localStorage.setItem('darkMode', document.body.classList.contains('dark'));
-    document.querySelector('.dark-mode-toggle').textContent =
-        document.body.classList.contains('dark') ? '☀️' : '🌙';
-}
-window.onload = function() {
-    if(localStorage.getItem('darkMode') === 'true') {
-        document.body.classList.add('dark');
-        document.querySelector('.dark-mode-toggle').textContent = '☀️';
-    }
-};
+
